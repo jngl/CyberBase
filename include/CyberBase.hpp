bdf5a591f8a1e9cac2cb909f486a05c85ed93a48
jngl <jngl92@gmail.com>
2018-08-31 16:27:22 +0200

    remove tuple3

--- include/CyberBase.hpp
+++ include/CyberBase.hpp
@@ -7,6 +7,5 @@
 #include <CyberBase/Stream/StreamFilter.hpp>
 #include <CyberBase/Stream/StreamMap.hpp>
 #include <CyberBase/Stream/StreamVector.hpp>
-#include <CyberBase/Utils/Tuple3.hpp>
 
 #endif // CYBERBASE_HPP
