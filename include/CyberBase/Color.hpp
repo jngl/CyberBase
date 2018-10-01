b71d103e1d3703baedda4be646ee7b546eccd967
jngl <jngl92@gmail.com>
2018-10-01 17:43:05 +0200

    add color

---
+++ include/CyberBase/Color.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+namespace cb 
+{
+    template<class T>
+    class ColorRGB{
+    public:
+        T red, green, blue;
+    };
+
+    template<class T>
+    class ColorRGBA{
+    public:
+        T red, green, blue, alpha;
+    };
+
+    typedef ColorRGB<float> ColorRGBf;//float(0, 1)
+    typedef ColorRGB<unsigned int> ColorRGBu8;//float(0, 255)
+
+    typedef ColorRGBA<float> ColorRGBAf;//float(0, 1)
+    typedef ColorRGBA<unsigned int> ColorRGBAu8;//float(0, 255)
+}
