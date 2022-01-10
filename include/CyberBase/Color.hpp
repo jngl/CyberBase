901133d8806d38993c84114c8b72901fc28ec638
jngl <jngl92@gmail.com>
2022-01-10 22:50:08 +0100

    Bettre cb::Color

--- include/CyberBase/Color.hpp
+++ include/CyberBase/Color.hpp
@@ -20,6 +20,8 @@ namespace cb
     typedef ColorRGBA<float> ColorRGBAf;//float(0, 1)
     typedef ColorRGBA<unsigned int> ColorRGBAu8;//float(0, 255)
 
-    constexpr ColorRGBAu8 BLACK_u8{0,0,0,255};
-    constexpr ColorRGBAu8 RED_u8{255,0,0,255};
+    using Color = ColorRGBAu8;
+
+    constexpr Color BLACK{0,0,0,255};
+    constexpr Color RED{255,0,0,255};
 }
